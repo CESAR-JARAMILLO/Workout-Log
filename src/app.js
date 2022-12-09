cc2f1f2a5e226b22514e70b3341230e96f229dca
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-08 17:20:20 -0800

    post edit state & update

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ ui.excerciseSubmit.addEventListener('click', addExcercise)
 // Listen for delete
 ui.excercises.addEventListener('click', deleteExcercise)
 
+// Listen for edit state change
+ui.excercises.addEventListener('click', enableEdit)
+
+// Listen for cancel edit
+document.querySelector('.card-form').addEventListener('click', cancelEdit)
+
 
 // Get Excercises
 function getExcercises() {
@@ -24,20 +30,39 @@ function addExcercise() {
   const name = ui.excerciseName.value
   const sets = ui.sets.value
   const reps = ui.reps.value
-  
+  const id = ui.idInput
+
   const data = {
     name,
     sets,
     reps
   }
 
-  http.post('http://localhost:3000/Excercises', data)
-    .then(data => {
-      ui.showAlert('Excercise Added', 'alert alert-success')
-      ui.clearFields()
-      getExcercises()
-    })
-    .catch(err => console.log(err))
+  // Validate input
+  if(name === '' || sets === '' || reps === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger')
+  } else {
+    // Check for ID
+    if(id === '') {
+      // Create excercise
+      http.post('http://localhost:3000/Excercises', data)
+      .then(data => {
+        ui.showAlert('Excercise Added', 'alert alert-success')
+        ui.clearFields()
+        getExcercises()
+      })
+      .catch(err => console.log(err))
+    } else {
+      // Update excercise
+      http.put(`http://localhost:3000/Excercises/${id}`, data)
+      .then(data => {
+        ui.showAlert('Excercise Updated', 'alert alert-success')
+        ui.changeFormState('add')
+        getExcercises()
+      })
+      .catch(err => console.log(err))
+    }
+  }
 }
 
 // Remove excercise
@@ -53,6 +78,41 @@ function deleteExcercise(e) {
         })
         .catch(err => console.log(err))
     }
+  }
+
+  e.preventDefault()
+}
+
+// Enable Edit State - Using Event Delegation
+function enableEdit(e) {
+  if(e.target.classList.contains('edit')) {
+
+    const id = e.target.dataset.id
+
+    const excerciseName = e.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+
+    const sets = e.target.previousElementSibling.previousElementSibling.textContent
+
+    const reps = e.target.previousElementSibling.textContent
+
+    const data = {
+      id,
+      excerciseName,
+      sets,
+      reps
+    }
+
+    // Fill form with current excercise
+    ui.fillForm(data)
+  }
+
+  e.preventDefault()
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+  if(e.target.classList.contains('excercise-cancel')) {
+    ui.changeFormState('add')
   }
 
   e.preventDefault()
--- src/ui.js
+++ src/ui.js
@@ -5,10 +5,10 @@ class UI {
     this.sets = document.querySelector('#sets')
     this.reps = document.querySelector('#reps')
     this.idInput = document.querySelector('#id')
-    this.idInput = document.querySelector('#id')
     this.excerciseSubmit = document.querySelector('.excercise-submit')
   }
 
+  // Show excercises
   showExcercises(excercises) {
     let output = ''
 
@@ -33,6 +33,7 @@ class UI {
     this.excercises.innerHTML = output
   }
 
+  // Show alert
   showAlert(message, className) {
     this.clearAlert()
 
@@ -53,6 +54,7 @@ class UI {
   }, 3000)
   }
 
+  // Clear alert
   clearAlert() {
     const currentAlert = document.querySelector('.alert')
 
@@ -61,10 +63,58 @@ class UI {
     }
   }
 
+  // Clear all fields
   clearFields() {
     this.excerciseName.value = ''
     this.sets.value = ''
     this.reps.value = ''
+  }
+
+  // Fill form to edit
+  fillForm(data) {
+    this.excerciseName.value = data.excerciseName
+    this.sets.value = data.sets
+    this.reps.value = data.reps
+    this.idInput = data.id
+
+    this.changeFormState('edit')
+  }
+
+  // Clear ID hidden value
+  clearIdInput() {
+    this.idInput = ''
+  }
+
+  // Change the form state
+  changeFormState(type) {
+    if(type === 'edit') {
+      this.excerciseSubmit.textContent = 'Update Excercise'
+      this.excerciseSubmit.className = 'excercise-submit btn btn-warning btn-block'
+
+      // Create cancel button
+      const button = document.createElement('button')
+      button.className = 'excercise-cancel btn btn-light btn-block'
+      button.appendChild(document.createTextNode('Cancel Edit'))
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form')
+      // get element to insert before
+      const formEnd = document.querySelector('.form-end')
+      // Insert button
+      cardForm.insertBefore(button, formEnd)
+    } else {
+      this.excerciseSubmit.textContent = 'Submit Excercise'
+      this.excerciseSubmit.className = 'excercise-submit btn btn-primary btn-block'
+
+      // Remove cancel btn if it's there
+      if(document.querySelector('.excercise-cancel')) {
+        document.querySelector('.excercise-cancel').remove()
+      }
+      // Clear id from hidden field
+      this.clearIdInput()
+      // Clear text
+      this.clearFields()
+    }
   }
 }
 
